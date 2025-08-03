ea786b7b14a79b29c8550f397ad2de680d07b907
Emilio Quinga <emilio.quinga22@gmail.com>
2025-08-03 14:51:45 -0500

    Examen 2 Maldonado_Motoche_Quinga

---
+++ librerias-usadas/examen2.js
@@ -0,0 +1,16 @@
+const suma = require('2025a-swgr2-ejq-suma');
+const resta = require('2025a-swgr2-ejq-resta');
+const multiplicacion = require('2025a-swgr2-pam-multiplicacion');
+const division = require('2025a-swgr2-axm-division');
+
+
+console.log('Suma:', suma.suma(2, 1));
+
+
+console.log('Resta:', resta.resta(3, 2));
+
+
+console.log('Multiplicación:', multiplicacion.multiplicacion(2, 5));
+
+
+console.log('División:', division.division(9, 3));
